7e88bc6e6a0caa5249db4f3e47c3b4d636d32c88
chc <99154196+ch77ancreant@users.noreply.github.com>
2022-09-19 16:42:39 +0800

    update scripts

--- 1/cvx_test.m
+++ 1/cvx_test.m
@@ -1,10 +1,11 @@
 clear;
 
-A = [1 3;1 -3;-1 0];
-b = [0.1;0.1;0.1];
-c = [1;2];
+A = [1 3; 1 -3; -1 0];
+b = [0.1; 0.1; 0.1];
+c = [1; 2];
 
 cvx_begin
         variable x(2,1)
         minimize( c.'*x - sum_log(-A*x+b) )
 cvx_end
+
